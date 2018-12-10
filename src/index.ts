0df899bbb2bda2e9ea936b160964ef8fc6c7f940
Gilad Gray <ggray@palantir.com>
2018-12-10 11:28:02 -0800

    init

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Gilad Gray [11:07 AM]
+// 1. for each icon: svg -> png -> average
+// 2. for each pixel/sample of piotr: find most suitable icon, maybe colorize (edited)
+
+// Bill Dwyer [11:08 AM]
+// svg -> png
+// for each png, create an array of `{avgLightness , iconName }`. sort by `avgLightness`
+// for each piotr pixel, binary search to find nearest `avgLightness`
+// colorize icon using hue/sat from piotr pixel
+
+// Gilad Gray [11:10 AM]
+// perhaps 1 “piotr-pixel” `pp` == 16x16 “actual pixels”
+
+// Bill Dwyer [11:10 AM]
+// now it gets more interesting
+// and you probably actually want to match 16x16 piotr pixels with 16x16 icon pixels
+// for that i would do the `sqrt(sum((piotr.lightness - icon.lightness)^2))`
